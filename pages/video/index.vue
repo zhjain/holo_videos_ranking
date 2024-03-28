8bd3d8b036f9d5cd4379bd6a4c852394b74f3acb
zhou jian <1099738693@qq.com>
2024-03-28 09:15:46 +0800

    .

---
+++ pages/video/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data }: any = await useFetch('/api/videos', {
+    method: 'GET'
+})
+// console.log(count.value as any,'res');
+
+
+</script>
+
+<template>
+    <div>Video</div>
+    <div>
+        <div v-for="item in data?.data || []">
+            {{ item.channel_name }}
+        </div>
+    </div>
+</template>
