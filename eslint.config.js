b3c5764292686808671b839476897c8db18aca49
Milky <1099738693@qq.com>
2024-11-21 16:04:29 +0800

    feat: 修改readme和icon

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import prettier from 'eslint-config-prettier'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import js from '@eslint/js'
 import svelte from 'eslint-plugin-svelte'
 import globals from 'globals'
@@ -11,6 +12,12 @@ export default ts.config(
     prettier,
     ...svelte.configs['flat/prettier'],
     {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,svelte}'],
+        rules: {
+            'no-console': 'error',
+        },
+    },
+    {
         languageOptions: {
             globals: {
                 ...globals.browser,
@@ -19,15 +26,23 @@ export default ts.config(
         }
     },
     {
-        files: ['**/*.svelte'],
+        plugins: {
+            svelte: svelte,
+        },
+        files: ['**/*.svelte', '*.svelte'],
 
         languageOptions: {
             parserOptions: {
-                parser: ts.parser
+                parser: ts.parser,
+                ecmaVersion: 'latest',
+                ecmaFeatures: {
+                    jsx: false,
+                },
             }
         }
     },
     {
         ignores: ['build/', '.svelte-kit/', 'dist/']
-    }
+    },
+    prettierRecommended,
 )
